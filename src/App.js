500aa54dcaa7d50f070202d831a9cbff178c07b3
SeanKim <sean.kim84@gmail.com>
2018-10-19 13:06:00 +0700

    Installing the Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>  
+      <Provider store={store}>
+        <div> 
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>  
+      </Provider>
     );
   }
 }
