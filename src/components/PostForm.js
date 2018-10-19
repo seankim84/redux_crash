669b839df7d96b58809de1d91c8fb1153c01b9ee
SeanKim <sean.kim84@gmail.com>
2018-10-19 10:14:55 +0700

    Adding the Submit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -7,28 +7,47 @@ class PostForm extends Component {
                 title: '',
                 body: ''
             };
-        this.onChange = this.onChange.bind(this);
+        this.setChange = this.onChange.bind(this);
+        this.setSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
 
+    onSubmit(e) {
+            e.preventDefault();
+            const post = {
+                title: this.state.title,
+                body: this.state.body   
+            }
+
+            fetch('https://jsonplaceholder.typicode.com/posts',{
+                method: 'POST',
+                headers: {
+                   'content-type': 'application/json'
+                },
+                body: JSON.stringify(post)
+            })
+            .then(res => res.json())
+            .then(data => console.log(data));
+        }
+
   render() {
     return (
       <div>
         <h1>Add the Post</h1>
-        <form>
+        <form onSubmit={this.setSubmit}>
             <div>
                 <label>Title:{this.state.title}</label>
                 <br />
-                <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
+                <input type="text" name="title" onChange={this.setChange} value={this.state.title} />
             </div>
             <br />
             <div>
                 <label>Body:{this.state.body}</label>
                 <br />
-                <input type="text" name="body" onChange={this.onChange} value={this.state.body} />
+                <input type="text" name="body" onChange={this.setChange} value={this.state.body} />
             </div>
                 <br />
                 <button type='submit'>Submit</button>
